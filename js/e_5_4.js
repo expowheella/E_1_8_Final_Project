c1d40c96c5316a3ffaa64687de36c3c892ba3411
Bulat <bulat_man@mail.ru>
2022-11-12 18:16:25 +0300

    e_5_3__e_5_4

---
+++ js/e_5_4.js
@@ -0,0 +1,9 @@
+/** Работа с событиями */
+
+function clickMe() {
+    alert('Clicked!')
+}
+
+// Вызов функции clickMe из HTML:
+// <button id="mybutton2">Button 2</button>
+mybutton2.onclick = clickMe
